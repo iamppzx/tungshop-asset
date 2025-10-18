3a6d712127ee8e1030e344c74f6475fa5d2a5e90
iamppzx <87563013+iamppzx@users.noreply.github.com>
2025-10-18 07:14:37 +0700

    Create cctownip.js

---
+++ cctownip.js
@@ -0,0 +1,23 @@
+{
+  "author": {
+    "name": "CC System",
+    "icon_url": "http://media.discordapp.net/attachments/1425940343203037295/1425959566029619230/logocctown_x2.png?ex=68f407d5&is=68f2b655&hm=783631d03d2ebe45a74da39e057253b4d0f1cc929a74f672cbb328b194aaf151&format=webp&quality=lossless&width=873&height=873&"
+  },
+  "title": "**CC TOWN SERVER IP**",
+  "url": "https://cfx.re/join/7gvv4e",
+  "fields": [
+    {
+      "name": "ลิ้ง Discord CC TOWN",
+      "value": "https://discord.gg/stkxAt4PJP\n\nกด F8 วาง IP และกด Enter เพื่อเข้าสู่ประเทศ\n```connect 154.84.153.133```\n\n----- **หรือ** กด [__**เชื่อมต่อ**__](https://cfx.re/join/7gvv4e) เพื่อเข้าสู่เซิฟเวอร์โดยตรง  -----",
+      "inline": true
+    }
+  ],
+  "image": {
+    "url": "https://media.discordapp.net/attachments/1425940343203037295/1425959566029619230/logocctown_x2.png?ex=68f407d5&is=68f2b655&hm=783631d03d2ebe45a74da39e057253b4d0f1cc929a74f672cbb328b194aaf151&=&format=webp&quality=lossless&width=873&height=873"
+  },
+  "color": "#00ffff",
+  "footer": {
+    "text": "Have Fun! - CCTeam",
+    "icon_url": "http://media.discordapp.net/attachments/1425940343203037295/1425959566029619230/logocctown_x2.png?ex=68f407d5&is=68f2b655&hm=783631d03d2ebe45a74da39e057253b4d0f1cc929a74f672cbb328b194aaf151&format=webp&quality=lossless&width=873&height=873&"
+  }
+}
